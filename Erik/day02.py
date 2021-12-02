995e1ca750d97b7d5343dafd45611e4baa1c70c7
Egendor <erik_jansson123@hotmail.com>
2021-12-02 07:06:04 +0100

    Dag 2, med läsbar output

--- Erik/day02.py
+++ Erik/day02.py
@@ -3,7 +3,6 @@ with open("Erik/inputs/input02.txt", "r") as f:
     for line in f:
         directions.append(line.strip())
 
-
 depth = 0
 pos = 0
 
@@ -22,5 +21,5 @@ for instr in directions:
         pos += val
         depth_two += aim * val
 
-print(depth, pos, depth*pos)
-print(depth_two, pos, depth_two * pos)
+print(f"Part 1: Curren depth is {depth} and current horizontal position is {pos}. Answer: {depth*pos}")
+print(f"Part 2: Curren depth is {depth_two} and current horizontal position is {pos}. Answer: {depth_two*pos}")
