37dcd32be8d7a2a422bbfb88a141f6428ad3c49d
Egendor <erik_jansson123@hotmail.com>
2021-12-02 07:11:26 +0100

    Dag 2, onödiga ändringar för minimalt snabbare kod

--- Erik/day02.py
+++ Erik/day02.py
@@ -3,23 +3,20 @@ with open("Erik/inputs/input02.txt", "r") as f:
     for line in f:
         directions.append(line.strip())
 
-depth = 0
+depth = 0 #Kommer alltid att ha samma värde som "aim"
 pos = 0
 
-aim = 0
 depth_two = 0
 
 for instr in directions:
     val = int(instr.split(" ")[1])
     if "up" in instr:
         depth -= val
-        aim -= val
     elif "down" in instr:
         depth += val
-        aim += val
     elif "forward" in instr:
         pos += val
-        depth_two += aim * val
+        depth_two += depth * val
 
 print(f"Part 1: Curren depth is {depth} and current horizontal position is {pos}. Answer: {depth*pos}")
 print(f"Part 2: Curren depth is {depth_two} and current horizontal position is {pos}. Answer: {depth_two*pos}")
