1f086560498128ee56a94db3b67b1c08399e8025
Egendor <erik_jansson123@hotmail.com>
2021-12-03 17:20:40 +0100

    Formatterad output, löser nu del 2 två gånger

--- Erik/day03.py
+++ Erik/day03.py
@@ -17,7 +17,8 @@ for i in range(0, len(diag_report[0])):
         gamma_rate += "0"
         epsilon_rate += "1"
 
-print(int(gamma_rate, 2) * int(epsilon_rate, 2))
+print(f"The gamma rate is {int(gamma_rate, 2)} and the epsilon rate is {int(epsilon_rate, 2)}")
+print(f"Part 1 answer (product): {int(gamma_rate, 2) * int(epsilon_rate, 2)}")
 
 
 # Part 2
@@ -50,6 +51,30 @@ for i in range(0, len(diag_report[0])):
         least_common = "1"
     co_scrubber_rate = [x for x in co_scrubber_rate if x[i] == least_common]
 
-print(oxygen_gen_rate, co_scrubber_rate)
-print(int(oxygen_gen_rate[0], 2), int(co_scrubber_rate[0],2))
-print(int(oxygen_gen_rate[0], 2) * int(co_scrubber_rate[0],2))
+print(f"\nThe O2 gen rate is {int(oxygen_gen_rate[0], 2)}, the CO2 scrub rate is {int(co_scrubber_rate[0],2)}")
+print(f"Part 2 answer (product): {int(oxygen_gen_rate[0], 2) * int(co_scrubber_rate[0],2)}")
+
+
+oxygen_gen_rate = diag_report.copy() # Most common
+co_scrubber_rate = diag_report.copy() # Least common
+
+for i in range(0,len(diag_report[0])):
+    one_count = [x[i] for x in oxygen_gen_rate].count("1")
+    if one_count >= (len(oxygen_gen_rate) /2):
+        oxygen_gen_rate = [x for x in oxygen_gen_rate if x[i] == "1"]
+    elif one_count < (len(oxygen_gen_rate) /2):
+        oxygen_gen_rate = [x for x in oxygen_gen_rate if x[i] == "0"]
+    if len(oxygen_gen_rate) == 1:
+        break
+
+for i in range(0,len(diag_report[0])):
+    one_count = [x[i] for x in co_scrubber_rate].count("1")
+    if one_count < (len(co_scrubber_rate) /2):
+        co_scrubber_rate = [x for x in co_scrubber_rate if x[i] == "1"]
+    elif one_count >= (len(co_scrubber_rate) /2):
+        co_scrubber_rate = [x for x in co_scrubber_rate if x[i] == "0"]
+    if len(co_scrubber_rate) == 1:
+        break
+    
+print(f"\n\nThe O2 gen rate is {int(oxygen_gen_rate[0], 2)}, the CO2 scrub rate is {int(co_scrubber_rate[0],2)}")
+print(f"Part 2 answer (product): {int(oxygen_gen_rate[0], 2) * int(co_scrubber_rate[0],2)}")
