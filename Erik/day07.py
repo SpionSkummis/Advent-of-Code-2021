749db669d497fe5fd3361dbd7aa140b1658aa5b5
Egendor <erik_jansson123@hotmail.com>
2021-12-07 20:12:10 +0100

    Dag 7 städad och uppsnabbad

--- Erik/day07.py
+++ Erik/day07.py
@@ -9,15 +9,20 @@ def calc_fuel(in_list, final_pos):
     return int(sum([abs(final_pos - x) for x in in_list]))
 
 def calc_fuel2(in_list, final_pos):
+    #Den här funktionen får stå kvar så att jag får skämmas
     return sum([sum(range(abs(final_pos-x)+1)) for x in in_list])
     #Eller om man vill kunna läsa det:
     #crab_ranges = [range(abs(final_pos-x)+1)) for x in in_list]
     #return sum([sum(x) for x in crab_ranges])
+
+def calc_fuel3(in_list, final_pos):
+    return int(sum([(abs(final_pos-x) * (abs(final_pos-x)+1)) for x in in_list])/2)
+    
     
 part1 = calc_fuel(crabsubs, crab_median)
 print(f"Part 1 total fuel usage: {part1}")
 
-part2 = min([calc_fuel2(crabsubs, x) for x in range(crab_mean-7, crab_mean+8)])
+part2 = min([calc_fuel3(crabsubs, x) for x in range(crab_mean-2, crab_mean+3)])
 print(f"Part 2 total fuel usage: {part2}")
 
 #bruteforce
