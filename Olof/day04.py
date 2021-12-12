c5304a3c560a5deb9ba7af0f8a5ab74e34a20d3b
Goatcat <olof.jw.jansson@gmail.com>
2021-12-12 22:34:08 +0100

    Dag 04: Walk of shame (tack Jens)

--- Olof/day04.py
+++ Olof/day04.py
@@ -1,45 +1,35 @@
 import numpy as np
-# 27594 is too high
-
 print('Advent of Code 2021 - December 04 - Olof')
+
 input = open('Olof\day04_input.txt','r').read().splitlines()
-#--------------------------------------------------------------------------
-# Nummer som ska dras hamnar i numbers
 numbers = [int(a) for a in list(input[0].split(','))]
-print(numbers)
-#--------------------------------------------------------------------------
-# Spelbrickor hamnar i boards. 100 st. Har försökt kontrollera att alla även är 5x5 osv. Tycker det ser rätt ut
-boards = []
-board = []
+board, boards = [], []
 for i in range(1, len(input)):
     if len(input[i].strip().split()) == 5:
         board.append(input[i].strip().split())
     if len(board) == 5:
         boards.append(np.array(board).astype(int))
         board = []
-#--------------------------------------------------------------------------
-# Funktion som kontrollerar om en bricka har bingo.
-# Givet en rad eller kolumn, count de som är -1 (<0). Om vi hittar 5 st -1 så har vi bingo. 
+
 def hasBingo(board:np.ndarray):
     for i in range(5):
-        if np.count_nonzero(board[i, 0:5] < 0) == 5: #kolla rad-bingo
-            return True
-        if np.count_nonzero(board[0:5, i] < 0) == 5: #kolla kolumn-bingo
+        if np.count_nonzero(board[i, 0:5] < 0) == 5 or np.count_nonzero(board[0:5, i] < 0) == 5:
             return True
     return False
-#--------------------------------------------------------------------------
-# Bingo-loopen:
-# Loopa så länge det finns nummer kvar att dra
-while len(numbers) > 0:
-    nbr = numbers.pop(0)
-    print('Drawing number: ' + str(nbr))
-  
-    # Gå igenom alla boards och sätt -1 på alla ställen som den dragna siffran står
-    for board in boards:
-        board[board == nbr] = -1
 
-    if hasBingo(board): #hasBingo egen funktion
-        print('BINGO')        
-        print(board)
-        print('Score: ' + str(nbr * np.sum(board[board>0])))
-#--------------------------------------------------------------------------
+part1, part2 = 0, 0
+while part1 == 0 or part2 == 0: 
+    drawn = numbers.pop(0)
+    nonBingoBoards = []
+    for board in boards:
+        board[board == drawn] = -1
+        if hasBingo(board):
+            if part1 == 0:
+                print('Part 1: ' + str(drawn * np.sum(board[board>0])))
+                part1 = 1
+            if len(boards) == 1: # sista brickan kvar
+                print('Part 2: ' + str(drawn * np.sum(board[board>0])))
+                part2 = 1
+        else:
+            nonBingoBoards.append(board) # nonBingoBoards går vidare till nästa runda
+    boards = nonBingoBoards
